68d84eb231cd72dbb41143b5206f0ff90e1e119a
Madelyn Eriksen <hello@madelyneriksen.com>
2019-11-02 12:41:02 -0700

    Integrated HMR with backend.

--- {{cookiecutter.project_package}}/{{cookiecutter.project_package}}/urls.py
+++ {{cookiecutter.project_package}}/{{cookiecutter.project_package}}/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from .router import router
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="base.html")),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("api/", include(router.urls)),
