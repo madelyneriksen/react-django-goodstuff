912a58d8c462feb31c04a6fa632e9159ad0037e4
Madelyn Eriksen <hello@madelyneriksen.com>
2019-11-02 11:18:43 -0700

    Added basic React entrypoint.

---
+++ {{cookiecutter.project_package}}/frontend/src/App.jsx
@@ -0,0 +1,8 @@
+// Main application entrypoint.
+
+
+import React from 'react';
+
+const App = () => <p>Hello world!</p>;
+
+export default App;
---
+++ {{cookiecutter.project_package}}/frontend/src/__tests__/testApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from '../App';
+
+
+describe('App Component', () => {
+  it('renders without crashing', () => {
+    shallow(<App />)
+  })
+})
